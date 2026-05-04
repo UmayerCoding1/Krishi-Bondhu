94aebec8c8e60dbfda44ff083ddc7cccf7fb1544
umayerCoding1 <umayerpro175@gmail.com>
2026-05-04 10:10:22 +0600

    feat: add verify-page component and remove commented-out API rewrites in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,7 @@ const nextConfig: NextConfig = {
         hostname: '**', // allows all hostnames
       },
     ],
-  },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       // destination: 'https://ks-auth-server.vercel.app/api/:path*', //production
-  //       destination: 'http://localhost:5000/api/:path*', //development
-  //     },
-  //   ];
-  // },
+  }
 };
 
 export default nextConfig;
