1fb44200e75007feb90798b489b08c3515fe7438
umayerCoding1 <umayerpro175@gmail.com>
2026-05-11 11:07:31 +0600

    feat: add setting component with tabbed navigation for account, notification, and danger alert sections

--- components/setting.tsx
+++ components/setting.tsx
@@ -7,10 +7,23 @@ import { TabsContentWrapper } from './tabs-content-wrapper';
 import { AccountSetting } from './account-setting';
 import { NotificationSetting } from './notification-setting';
 import { DangerAlertSetting } from './danger-alert';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export const Setting = () => {
-    const [currentTab, setCurrentTab] = useState("notification");
+    const [currentTab, setCurrentTab] = useState("account");
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    useEffect(() => {
+        // router.replace(`/settings?tab=${currentTab}`)
+        const tab = searchParams.get("tab");
+        if (tab) {
+            setCurrentTab(tab);
+        }
+
+    }, [searchParams,]);
+
     return (
         <div>
             <div className='w-full flex items-center justify-center px-4'>
@@ -24,7 +37,10 @@ export const Setting = () => {
                 <div className=' max-w-7xl mx-auto border-l border-r border-neutral-300 lg:h-screen  '>
                     {/* tab */}
                     <div className='p-2 '>
-                        <Tabs value={currentTab} onValueChange={setCurrentTab} className="w-full">
+                        <Tabs value={currentTab} onValueChange={(value) => {
+                            setCurrentTab(value);
+                            router.push(`/settings?tab=${value}`)
+                        }} className="w-full">
                             <TabsList className='gap-10'>
                                 <TabsTrigger value="account">অ্যাকাউন্ট</TabsTrigger>
                                 <TabsTrigger value="notification">নোটিফিকেশন</TabsTrigger>
